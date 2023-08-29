3b6e3fe2c3aa8f6471b6f183ca12144617d3ebb4
Jaycee Buarao <jayceebuarao@gmail.com>
2023-08-28 22:10:11 -0700

    create enum Category

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,16 +2,21 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid(); //dart package that generate unique IDs
 
+//enums are custom type
+enum Category { food, travel, leisure, work }
+
 //data model
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = uuid.v4(); //Initializer list used for non function arguments
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
