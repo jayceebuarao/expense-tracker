b87d0d0647ab06ec8ad4fb15014dbe885f74ceb1
Jaycee Buarao <jayceebuarao@gmail.com>
2023-08-28 22:04:56 -0700

    add comments

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,13 +1,14 @@
 import 'package:uuid/uuid.dart';
 
-const uuid = Uuid();
+const uuid = Uuid(); //dart package that generate unique IDs
 
+//data model
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
-  }) : id = uuid.v4();
+  }) : id = uuid.v4(); //Initializer list used for non function arguments
 
   final String id;
   final String title;
