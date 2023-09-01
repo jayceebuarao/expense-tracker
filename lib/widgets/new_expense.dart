7dee0066a2ac9b749ae926d1774a08d047311425
Jaycee Buarao <jayceebuarao@gmail.com>
2023-08-31 19:52:13 -0700

    create new input form

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,18 +8,57 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _costController = TextEditingController();
+
+  @override
+  void dispose() {
+    //called after NewExpense Widget is closed
+    _titleController
+        .dispose(); //TextEditingController must be disposed as it is stored in memory after use
+    _costController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.all(20),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _costController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\$',
+              label: Text('Cost Amount'),
+            ),
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  print('Cancel');
+                },
+                child: const Text('Cancel'),
+              ),
+              Spacer(),
+              ElevatedButton(
+                onPressed: () {
+                  print('Title: ${_titleController.text}');
+                  print('Cost Amount: ${_costController.text}');
+                },
+                child: const Text('Save'),
+              )
+            ],
+          )
         ],
       ),
     );
