9944ffa374db08659171bb46903c162fb2843f64
Jaycee Buarao <jayceebuarao@gmail.com>
2023-09-04 19:21:51 -0700

    adjust theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ import 'package:expense_tracker/widgets/expenses.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      //entire app theme modified through theme property in MaterialApp() class
+      //but directly modifying properties inside on ThemeData() class tells flutter that everything will be customized
+      //To work around that and if preferred to keep other default properties use '.copywith' constructor
+      theme: ThemeData().copyWith(useMaterial3: true),
       home: const Expenses(),
     ),
   );
